fd71d34c369a0803d2fcd1cb20ad846d02fc48a0
Teu Nome <teuemail@exemplo.com>
2026-03-22 16:02:11 +0200

    fix: update trading mobile and desktop components

--- src/app/trading/desktop/tradingDesktop.tsx
+++ src/app/trading/desktop/tradingDesktop.tsx
@@ -1,25 +1,25 @@
 import TopBar from "../components/topBar";
 import OrderBook from "../components/OrderBook";
-import ChartArea from "../components/ChartArea";
-import TradePanel from "../components/TradePanel";
+import ChartArea from "../components/chartArea";
+import TradePanel from "../components/tradePanel";
 
 
 export default function TradingDesktop() {
   return (
     <div className="h-screen bg-[#0b0e14] text-white flex flex-col">
-     
+      <TopBar />
 
       <div className="flex flex-1 overflow-hidden">
         <div className="w-[320px] border-r border-white/10">
-          
+          <OrderBook />
         </div>
 
         <div className="flex-1">
-         
+          <ChartArea />
         </div>
 
         <div className="w-[360px] border-l border-white/10">
-          
+          <TradePanel />
         </div>
       </div>
     </div>
--- src/app/trading/mobile/tradingMobile.tsx
+++ src/app/trading/mobile/tradingMobile.tsx
@@ -1,11 +1,13 @@
-import ChartArea from "../components/ChartArea";
-import TradePanel from "../components/TradePanel";
-import MarketStats from "../components/MarketStats";
+import ChartArea from "../components/chartArea";
+import TradePanel from "../components/tradePanel";
+import MarketStats from "../components/marketStats";
 
 export default function TradingMobile() {
   return (
     <div className="min-h-screen bg-[#0b0e14] text-white flex flex-col">
-      
+      <MarketStats />
+      <ChartArea />
+      <TradePanel />
     </div>
   );
 }
